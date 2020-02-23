de2bd272f57571b650470302577ea4823cdd94c7
preyansh98 <preyansh98@gmail.com>
2020-02-23 13:20:46 -0500

    fixy

--- src/services/article.service.ts
+++ src/services/article.service.ts
@@ -6,41 +6,39 @@ import mongoose from "mongoose";
 export class ArtiService {
   public welcomeMessage(req: Request, res: Response) {
     let welcomeMessage = {
-        "status" : "Up"
-    };      
+      "status": "Up"
+    };
     res.status(200).send(welcomeMessage);
   }
 
   public addNewArticle(req: Request, res: Response) {
     const newArticle = new Article(req.body);
-    if(!req.body.title || !req.body.subtitle || !req.body.body || !req.body.author){
-      res.status(400); 
-      return
-    }
     newArticle.save((error: Error, Article: any) => {
       if (error) {
-        res.send(error);
-      }
-      res.json(Article);
+        if (!req.body.title || !req.body.subtitle || !req.body.body || !req.body.author) {
+          res.status(400).send("Invalid Article!");
+        } else
+          res.send(error);
+      } else
+        res.json(Article);
     });
   }
 
   public getArticle(req: Request, res: Response) {
-      const ArticleID = req.params.articleId; 
-      Article.findById(ArticleID, (error: Error, Article: any) => {
-          if(error){
-            if(req.params.articleId.length < 12){
-              res.status(404).send("No such article!"); 
-            }
-            else {
-              res.status(400).send("Article id wrong format")
-            }
-          }
-          else{
-              res.status(200).json(Article); 
-              console.log("returning");
-          }
-      });
+    const ArticleID = req.params.articleId;
+    Article.findById(ArticleID, (error: Error, Article: any) => {
+      if (error) {
+        if (req.params.articleId.length < 12) {
+          res.status(404).send("No such article!");
+        }
+        else {
+          res.status(400).send("Article id wrong format")
+        }
+      }
+      else {
+        res.status(200).json(Article);
+      }
+    });
   }
 
   public getAllArticle(req: Request, res: Response) {
@@ -53,39 +51,38 @@ export class ArtiService {
   }
 
   public deleteArticle(req: Request, res: Response) {
-    if(!req.params.articleId || req.params.articleId.length < 12){
-        res.status(400);
-        return 
-    }
     const ArticleID = req.params.articleId;
     Article.findByIdAndDelete(ArticleID, (error: Error, deleted: any) => {
       if (error) {
-        res.status(404).send(error);
-      }
-      res.status(200).json(deleted);
+        if (!req.params.articleId || req.params.articleId.length < 12)
+          res.status(400).send("Invalid ID");
+        else
+          res.status(404).send(error);
+      } else
+        res.status(200).json(deleted);
     });
   }
 
   public updateArticle(req: Request, res: Response) {
-    if(!req.params.articleId || req.params.articleId.length < 12){
-        res.status(400); 
-        return
-    }
     const ArticleId = req.params.articleId;
     Article.findByIdAndUpdate(
       ArticleId,
       req.body,
       (error: Error, Article: any) => {
         if (error) {
-          res.status(404).send(error);
-          return
+          if (!req.params.articleId || req.params.articleId.length < 12)
+            res.status(400).send("Invalid ID");
+          else
+            res.status(404).send(error);
         }
-        Article.findById(ArticleId, (error: Error, Article: any) => {
-            if(error)
-                res.status(404).send("No such article!"); 
+        else {
+          Article.findById(ArticleId, (error: Error, Article: any) => {
+            if (error)
+              res.status(404).send("No such article!");
             else
-                res.status(200).json(Article); 
+              res.status(200).json(Article);
           });
+        }
       }
     );
   }
